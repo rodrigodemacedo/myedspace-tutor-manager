1681cab85d30fb9c468efa2ba2db288a80ca0ba8
Rodrigo de Macedo <rodrigo.macedo@fusiondms.com.br>
2024-09-02 17:13:36 -0300

    build:migrations - Create the migration to store data

---
+++ database/migrations/2024_09_02_195756_create_tutors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tutors', function (Blueprint $table) {
+            $table->id();
+            $table->string('avatar')->nullable(); // Tutor's avatar (can be null)
+            $table->string('name', 255); // Tutor's name
+            $table->string('email', 255)->unique(); // Tutor's email (must be unique)
+            $table->decimal('hourly_rate', 8, 2); // Tutor's hourly rate (2 decimal places)
+            $table->text('bio')->nullable(); // Tutor's biography (can be null)
+            $table->json('subjects'); // Subjects taught by the tutor (stored as JSON)
+            $table->timestamps(); // created_at and updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tutors');
+    }
+};
---
+++ database/migrations/2024_09_02_200231_create_student_tutor_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_tutor', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tutor_id')->constrained()->onDelete('cascade'); // FK for tutors
+            $table->foreignId('student_id')->constrained()->onDelete('cascade'); // FK for students
+            $table->timestamps(); // created_at e updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_tutor');
+    }
+};
